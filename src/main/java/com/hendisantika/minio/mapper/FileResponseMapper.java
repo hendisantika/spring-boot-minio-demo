779ec798767eeb617e97cb68add0d9bc09114b18
hendisantika <hendisantika@yahoo.co.id>
2023-12-10 06:20:53 +0700

    Add FileResponseMapper🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/minio/mapper/FileResponseMapper.java
@@ -0,0 +1,35 @@
+package com.hendisantika.minio.mapper;
+
+import org.mapstruct.DecoratedWith;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-minio-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/10/23
+ * Time: 06:20
+ * To change this template use File | Settings | File Templates.
+ */
+@Mapper
+@DecoratedWith(FileResponseDecorator.class)
+public interface FileResponseMapper {
+
+    @Mapping(target = "stream", ignore = true)
+    @Mapping(target = "filename", ignore = true)
+    @Mapping(target = "fileSize", ignore = true)
+    @Mapping(target = "createdTime", ignore = true)
+    @Mapping(target = "contentType", ignore = true)
+    FileResponse fileAddResponse(ObjectStat objectStat);
+
+
+    @Mapping(target = "stream", ignore = true)
+    @Mapping(target = "filename", ignore = true)
+    @Mapping(target = "fileSize", ignore = true)
+    @Mapping(target = "createdTime", ignore = true)
+    @Mapping(target = "contentType", ignore = true)
+    FileResponse fileGetResponse(ObjectStat objectStat);
+}

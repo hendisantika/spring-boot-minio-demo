f326a1e9608214733b118ee5674d87002a5733cc
hendisantika <hendisantika@yahoo.co.id>
2023-12-10 06:24:25 +0700

    Add WebConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/minio/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.hendisantika.minio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-minio-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/10/23
+ * Time: 06:24
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorParameter(true)
+                .ignoreAcceptHeader(true)
+                .parameterName("mediaType")
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("hal", MediaTypes.HAL_JSON);
+    }
+}

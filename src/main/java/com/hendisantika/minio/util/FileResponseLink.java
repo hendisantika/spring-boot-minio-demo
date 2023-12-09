ecb0e2b2ae677bf35c90683545b4a59a541917d8
hendisantika <hendisantika@yahoo.co.id>
2023-12-10 06:22:44 +0700

    Add FileResponseLink🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/minio/util/FileResponseLink.java
@@ -0,0 +1,18 @@
+package com.hendisantika.minio.util;
+
+import com.hendisantika.minio.payload.FileResponse;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-minio-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/10/23
+ * Time: 06:22
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class FileResponseLink implements LinkUtils<FileResponse> {
+}

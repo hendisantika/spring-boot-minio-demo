e303a451d4ee363e594196c1eb8d45b9355e5145
hendisantika <hendisantika@yahoo.co.id>
2023-12-10 06:17:19 +0700

    Add ErrorHandler🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/minio/error/ErrorHandler.java
@@ -0,0 +1,20 @@
+package com.hendisantika.minio.error;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-minio-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/10/23
+ * Time: 06:17
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@RestControllerAdvice
+public class ErrorHandler {
+
+}

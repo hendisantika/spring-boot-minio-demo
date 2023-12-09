da85f5fc227e564dccf634cb85c57ba136eb9bf7
hendisantika <hendisantika@yahoo.co.id>
2023-12-10 06:08:03 +0700

    Init starter project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/minio/SpringBootMinioDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.minio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMinioDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMinioDemoApplication.class, args);
+    }
+
+}
